aa8e46edc73bbb1f0840891ea63a25535dd132b3
mselensk <matt.selensky@nrel.gov>
2025-02-11 15:13:17 -0700

    fix paths

--- run_tests.sh
+++ run_tests.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH -A hpcapps
+#SBATCH -A esifapps
 #SBATCH -p gpu-h100
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=4
 #SBATCH --gres=gpu:4
 #SBATCH --mem=0
-#SBATCH --time=00:10:00
-#SBATCH --job-name=nccl_slingshot_test
+#SBATCH --time=00:30:00
+#SBATCH --job-name=nccl_cxi_baremetal
 #SBATCH -o %j-%x.out
 
 module load cuda/12.3
 
-export NCCL_HOME=$PWD/nccl-ofi-plugin/install
+export NCCL_HOME=`pwd`/install
 export MPICH_GPU_SUPPORT_ENABLED=0
 
 export LD_LIBRARY_PATH=$NCCL_HOME/lib:$NCCL_HOME/plugin/lib:$LD_LIBRARY_PATH
@@ -23,5 +23,6 @@ export NCCL_SOCKET_IFNAME=hsn
 export NCCL_NET="AWS Libfabric"
 export NCCL_NET_GDR_LEVEL=PHB
 
+echo "NCCL_HOME is set to $NCCL_HOME"
 echo ========== RUNNING NCCL TESTS ==========
-srun $PWD/nccl-ofi-plugin/nccl-tests/build/all_reduce_perf -b 8 -e 4G -f 2
+srun $PWD/nccl-tests/build/all_reduce_perf -b 8 -e 4G -f 2
